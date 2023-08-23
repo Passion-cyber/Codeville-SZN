82b1128a2142b799b695a555cf3eb6ff15ee563e
Passion-cyber <chineduibelachi@gmail.com>
2023-08-23 14:09:39 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,29 @@
+let lastValue = "O";
+let display = document.getElementById("display");
+display.textContent = "Player 1's turn to play ";
+
+let buttons = document.querySelectorAll(".box");
+for (let button of buttons) {
+  button.addEventListener("click", () => {
+    if (button.textContent !== "") {
+      return;
+    }
+    if (lastValue === "O") {
+      button.textContent = "X";
+      lastValue = "X";
+      display.textContent = "player 2's turn to play";
+      checkStatus();
+    } else {
+      button.textContent = "O";
+      lastValue = "O";
+      display.textContent = "player 1's turn to play";
+      checkStatus();
+    }
+  });
+} 
+
+let restartButton = document.getElementById("controlBtn");
+
+restartButton.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
